59aac8483760323cc4fb45b4b68f3324fa68b128
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2010-10-17 13:20:21 +0100

    euler 3

--- Project Euler/euler3.clj
+++ Project Euler/euler3.clj
@@ -1,9 +1,9 @@
-(defn euler3 [lim]
-  (defn iter [x]
-    (cond (= (rem lim x) 0) x
-          :else (iter (- x 1)))
-  )
-  (iter (- lim 1))
-)
-
-(euler3 600851475143) ; stack overflow :(
+; adapted from http://grok-code.com/367/learning-clojure-with-project-euler/
+(defn factor [num cur]
+  (loop [num num, cur cur]
+    (if (= num cur)
+      num
+      (if (zero? (rem num cur))
+        (recur (/ num cur) cur)
+        (recur num (+ cur 2))))))
+(factor 600851475143 3)
